3d9bf63e57c5ced63741321c50f0339346845d8b
dinhtintr <learning.dinhtin@gmail.com>
2023-09-19 00:53:07 +0700

    update Homework Week 8

--- 3. Exercise Week 8/homeworkW8.cpp
+++ 3. Exercise Week 8/homeworkW8.cpp
@@ -31,8 +31,17 @@ void lay_phan_tu_tai_vitri(int* arr, int& n)
 {
     int findIndex;
     cout << "Nhap vi tri can lay: "; cin >> findIndex;
-    cout << arr[findIndex - 1];
+    cout << arr[findIndex - 1] << endl;
 }
+
+void swap(int& num1, int& num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void selectionSort(int *arr)
 
 int main()
 {
@@ -41,5 +50,21 @@ int main()
     nhap_Array(arr, n);
     xuat_Array(arr, n);
     lay_phan_tu_tai_vitri(arr, n);
+    cout << "Choose method to arrange array:" << endl;
+    cout << "1. Selection Sort" << endl;
+    cout << "2. Insertion Sort" << endl;
+    cout << "3. BubbleSort" << endl;
+    cout << "4. QuickSort" << endl;
+    cout << "5. HeapSort" << endl;
+    cout << "6. ShellSort" << endl;
+    cout << "7. RadixSort" << endl;
+    int choice ;
+    cout << "Enter your choice (1-7): "; cin >> choice;
+
+    switch (choice) {
+        case 1:
+
+    }
+
     delete[] arr;
 }
